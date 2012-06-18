b1debdd4ecb84366a7dcb1b09e629020a4d55949
sherpc <aleksandrsher@gmail.com>
2012-06-18 19:13:36 +0400

    Added FixnumBitExtension module

--- lab4/modules/gf.rb
+++ lab4/modules/gf.rb
@@ -6,4 +6,12 @@ module GF
     # If first bit is not 0 (a & 0x80 == 0), result is shift - m, where m = 0x011b, or m = x**8 + x**4 + x**3 + x + 1
     highbit == 0 ? shift : shift ^ 0x1b
   end
+
+  def self.mul a, b
+    for i in 0..7 do 
+      if (b & (1 << i)) != 0
+        
+      end
+    end
+  end
 end
