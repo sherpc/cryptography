a006b8fcb73c0919058ac668e956a7e2c7b551dd
sherpc <aleksandrsher@gmail.com>
2012-06-18 16:44:25 +0400

    Added GF.mul02 release

--- lab4/modules/gf.rb
+++ lab4/modules/gf.rb
@@ -1,5 +1,9 @@
 module GF
-  def self.mul a, b
-    a * b
+  def self.mul02 a
+    highbit = a & 0x80
+    # a * x, x = {02}, is equal to shift left. & with 0xff to cut overflows
+    shift = (a << 1) & 0xff
+    # If first bit is not 0 (a & 0x80 == 0), result is shift - m, where m = 0x011b, or m = x**8 + x**4 + x**3 + x + 1
+    highbit == 0 ? shift : shift ^ 0x1b
   end
 end
--- lab4/test/test_gf.rb
+++ lab4/test/test_gf.rb
@@ -4,7 +4,8 @@ require "./main.rb"
 class TestGF < Test::Unit::TestCase
 
   def test_mul02
-    assert_equal 0x63, GF.mul02(0x3c)
+    assert_equal 0x78, GF.mul02(0x3c)
+    assert_equal 0x63, GF.mul02(0xbc)
   end
 
   def test_mul
