ab0e18298bc4d33b9a9736426a78d642221e2743
sherpc <aleksandrsher@gmail.com>
2012-06-19 09:08:39 +0400

    RF: renamed AES to AESCore

--- lab4/test/test_aes.rb
+++ lab4/test/test_aes.rb
@@ -2,52 +2,5 @@ require "test/unit"
 require "./main.rb"
 
 class TestAES < Test::Unit::TestCase
-  def test_sbox
-    assert_equal 0x63, AES.sbox(0)
-    assert_equal 0x7c, AES.sbox(1)
-    assert_equal 0x76, AES.sbox(15)
-  end
-  
-  def test_inv_sbox
-    assert_equal 0x52, AES.inv_sbox(0)
-    assert_equal 0x09, AES.inv_sbox(1)
-  end
-  
-  def test_const
-    assert_equal 4, AES::Nk
-  end
-
-  def test_key_expansion
-    key = [216,87,142,223,132,88,206,6,251,197,187,118,165,140,92,164]
-    w = AES.key_expansion(key)
-    assert_equal 44, w.length
-  end
-
-  def test_state_from
-    input = [52, 49, 32, 56, 50, 32, 56, 49, 32, 50, 97, 32, 52, 48, 32, 51]
-    state = [[52, 50, 32, 52], [49, 32, 50, 48], [32, 56, 97, 32], [56, 49, 32, 51]]
-    assert_equal state, AES.state_from(input)
-  end
-
-  def test_output_from
-    state = [[52, 50, 32, 52], [49, 32, 50, 48], [32, 56, 97, 32], [56, 49, 32, 51]]
-    output = [52, 49, 32, 56, 50, 32, 56, 49, 32, 50, 97, 32, 52, 48, 32, 51]
-    assert_equal output, AES.output_from(state)
-  end
-
-  def test_scalar_mul
-    v1 = [2,3,1,1]
-    v2 = [23,106,234,73]
-    assert_equal 51, AES.scalar_mul(v1, v2)
-  end
-
-  def test_cipher
-    input = [95, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    key = [216,87,142,223,132,88,206,6,251,197,187,118,165,140,92,164]
-    w = AES.key_expansion(key)
-    cipher = [57, 92, 186, 30, 30, 209, 113, 117, 13, 128, 2, 213, 87, 232, 85, 238]
-    assert_equal cipher, AES.cipher(input, w)
-    assert_equal input, AES.inv_cipher(cipher, w)
-  end
 end
 
