43f9325acb18b1e97d1a623c9c5e79a2ea143068
sherpc <aleksandrsher@gmail.com>
2012-06-20 22:32:34 +0400

    Added lab3

---
+++ lab3/main.rb
@@ -0,0 +1,39 @@
+def make_sqrt_list
+  cache = { 1 => 1 }
+  last_key = 1
+  lambda { |x|
+    return cache if cache.has_value?(x)
+    while(last_key != x) do
+      last_key += 1
+      p last_key
+      cache[last_key * last_key] = last_key
+    end
+    cache
+  }
+end
+
+Sqrt_list = make_sqrt_list
+
+def is_sqrt? x, n
+  y2 = x ** 2 - n
+  Sqrt_list[Math.sqrt(y2).ceil][y2]
+end
+
+def factorize n
+  return 2, n / 2 if n % 2 == 0
+  x = Math.sqrt(n).floor
+  y = nil
+  while(not y)
+    x += 1
+    y = is_sqrt?(x, n)
+  end
+  return x+y, x-y
+end
+
+def assert n
+  x, y = factorize(n)
+  p n == (x * y)
+end
+assert 33
+assert 21
+assert 45
